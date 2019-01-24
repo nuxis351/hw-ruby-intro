c24bc99b081c00c6125282578d018abbd17fe661
Illya Yefimenko <iyefimenko@bearincorp.com>
2019-01-24 16:45:12 -0500

    part1 finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,29 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0) {|sum, n| sum + n}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  total = 0
+  if arr.count == 1
+    arr.first
+  else
+    sum arr.max(2)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false unless arr.count > 1
+  arr.each do |item|
+    new_arr = []
+    new_arr.replace(arr)
+    new_arr.delete_at(new_arr.index(item))
+    if new_arr.include? (n-item).abs
+      return true
+    end
+  end
+  return false
 end
 
 # Part 2
